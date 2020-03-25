c2483adb52c35bab1b4c6fec64fc49b833f610eb
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-03-25 21:49:45 +0200

    Create inner class FetchData create query for "Ecology" and display symbols on screen.

--- app/src/main/java/com/example/eco/AllAboutEcology.java
+++ app/src/main/java/com/example/eco/AllAboutEcology.java
@@ -39,7 +39,51 @@ public class AllAboutEcology extends AppCompatActivity {
         click = (Button) findViewById(R.id.get);
 
 
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FetchData process = new FetchData();
+                process.execute();
+            }
+        });
 
+    }
+
+    private class FetchData extends AsyncTask<Void, Void, Void> {
+
+        String data = "";
+        String parsedData = "";
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                URL url = new URL(
+                        "https://uk.wikipedia.org/w/api.php?action=parse&page=%D0%95%D0%BA%D0%BE%D0%BB%D0%BE%D0%B3%D1%96%D1%8F&prop=text&formatversion=2&format=json&section=1");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = "";
+
+                while (line != null){
+                    line = bufferedReader.readLine();
+                    data = data + line;
+                }
+
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            AllAboutEcology.data.setText(this.data);
+        }
     }
 
 }
