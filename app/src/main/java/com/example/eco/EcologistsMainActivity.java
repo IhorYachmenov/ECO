fff5215ecb8efa99ea360e6e9ab1b46af7839813
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-03-28 01:43:49 +0200

    Delete fragments, and list view adapters and chained .xml.

--- app/src/main/java/com/example/eco/EcologistsMainActivity.java
+++ app/src/main/java/com/example/eco/EcologistsMainActivity.java
@@ -1,6 +1,7 @@
 package com.example.eco;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,33 +9,38 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
+
 public class EcologistsMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scientists);
 
-        // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        // Create an adapter that knows which fragment should be shown on each page
-        /**
-         Since we modified the CategoryAdapter constructor, we also need to update the MainActivity (which uses that constructor).
-         When we create a CategoryAdapter, we pass in a Context (which is “this” or the activity) and the FragmentManager.
-         */
-        EcologistAdapter adapter = new EcologistAdapter(this, getSupportFragmentManager());
-
-        // Set the adapter onto the view pager
-        viewPager.setAdapter(adapter);
-
-        // Find the tab layout that shows the tabs
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-
-        // Connect the tab layout with the view pager. This will
-        //   1. Update the tab layout when the view pager is swiped
-        //   2. Update the view pager when a tab is selected
-        //   3. Set the tab layout's tab names with the view pager's adapter's titles
-        //      by calling onPageTitle()
-        tabLayout.setupWithViewPager(viewPager);
+//        setContentView(R.layout.scientists);
+//
+//        // Find the view pager that will allow the user to swipe between fragments
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+//
+//        // Create an adapter that knows which fragment should be shown on each page
+//        /**
+//         Since we modified the CategoryAdapter constructor, we also need to update the MainActivity (which uses that constructor).
+//         When we create a CategoryAdapter, we pass in a Context (which is “this” or the activity) and the FragmentManager.
+//         */
+//        EcologistAdapter adapter = new EcologistAdapter(this, getSupportFragmentManager());
+//
+//        // Set the adapter onto the view pager
+//        viewPager.setAdapter(adapter);
+//
+//        // Find the tab layout that shows the tabs
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+//
+//        // Connect the tab layout with the view pager. This will
+//        //   1. Update the tab layout when the view pager is swiped
+//        //   2. Update the view pager when a tab is selected
+//        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+//        //      by calling onPageTitle()
+//        tabLayout.setupWithViewPager(viewPager);
     }
 }
