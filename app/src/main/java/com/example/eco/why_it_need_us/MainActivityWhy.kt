cc8161baf1dbf2021b1996517d40756ab93fbd6c
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-04-20 17:04:16 +0300

    Created MotionLayout, added custom CollapsibleToolbar and other additional layouts.

--- app/src/main/java/com/example/eco/why_it_need_us/MainActivityWhy.kt
+++ app/src/main/java/com/example/eco/why_it_need_us/MainActivityWhy.kt
@@ -8,5 +8,6 @@ class MainActivityWhy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_why)
+        supportActionBar?.hide()
     }
 }
