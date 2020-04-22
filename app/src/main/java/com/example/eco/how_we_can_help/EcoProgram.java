2279f426bafc800de838c66a87e5fa8de6766b02
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-04-22 17:22:56 +0300

    Created different activity for different case states.

--- app/src/main/java/com/example/eco/how_we_can_help/EcoProgram.java
+++ app/src/main/java/com/example/eco/how_we_can_help/EcoProgram.java
@@ -2,7 +2,6 @@ package com.example.eco.how_we_can_help;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -12,29 +11,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.eco.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class ActivityMainHelp extends AppCompatActivity {
+public class EcoProgram extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_how_we_can_help);
+        setContentView(R.layout.activity_eco_program);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setItemIconTintList(null);
 
-        bottomNavigationView.setSelectedItemId(R.id.action_how);
+        bottomNavigationView.setSelectedItemId(R.id.action_program);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.action_how:
+                        startActivity(new Intent(getApplicationContext(), ActivityMainHelp.class));
+                        overridePendingTransition(0,0);
                         return true;
                     case R.id.action_program:
-                        startActivity(new Intent(getApplicationContext(), EcoProgram.class));
-                        overridePendingTransition(0,0);
                         return true;
                 }
 
@@ -42,6 +41,5 @@ public class ActivityMainHelp extends AppCompatActivity {
                 return false;
             }
         });
-
     }
 }
