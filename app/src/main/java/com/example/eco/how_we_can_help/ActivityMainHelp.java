0c755c3ca9e8cedbcdcc04fc1cf222a709c2bcdf
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-04-22 16:27:03 +0300

    Created BottomNavigationView, and was created selector with different image states for BottomNavigationView.

--- app/src/main/java/com/example/eco/how_we_can_help/ActivityMainHelp.java
+++ app/src/main/java/com/example/eco/how_we_can_help/ActivityMainHelp.java
@@ -1,17 +1,27 @@
 package com.example.eco.how_we_can_help;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eco.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ActivityMainHelp extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_how_we_can_help);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setItemIconTintList(null);
+
     }
 }
